c370404f1b273c00095a9bd5214ce13d655f59c6
Lucas Veslin <lucas.thibaud@hotmail.com>
2025-05-12 11:00:05 +0200

    First Commit

---
+++ src/main/java/utcapitole/miage/bloop/BloopApplication.java
@@ -0,0 +1,14 @@
+package utcapitole.miage.bloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloopApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/utcapitole/miage/bloop/BloopApplicationTests.java
@@ -0,0 +1,14 @@
+package utcapitole.miage.bloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// TODO document why this method is empty
+	}
+
+}
