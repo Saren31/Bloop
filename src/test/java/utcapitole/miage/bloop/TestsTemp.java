082957eb9f90d8019a61fa314447427e5ad13c1c
Lucas Veslin <lucas.thibaud@hotmail.com>
2025-05-12 11:42:31 +0200

    Add Sonarqloud4

---
+++ src/test/java/utcapitole/miage/bloop/TestsTemp.java
@@ -0,0 +1,13 @@
+package utcapitole.miage.bloop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestsTemp {
+
+    @Test
+    void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
