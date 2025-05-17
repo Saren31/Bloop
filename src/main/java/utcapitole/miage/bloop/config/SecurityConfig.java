82fcfd9ece8b95f5898ab4c2990a07f54077583d
Saren <lucas.thibaud@hotmail.com>
2025-05-17 19:15:56 +0200

    Correction token csrf

--- src/main/java/utcapitole/miage/bloop/config/SecurityConfig.java
+++ src/main/java/utcapitole/miage/bloop/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                //.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers(
                                 "/",                       // Page racine.
