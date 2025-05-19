113359e32286f5d31663e6ad5966132b01005989
Saren <lucas.thibaud@hotmail.com>
2025-05-19 18:41:59 +0200

    Bugfixe Utilisateur identification

--- src/main/java/utcapitole/miage/bloop/model/entity/Utilisateur.java
+++ src/main/java/utcapitole/miage/bloop/model/entity/Utilisateur.java
@@ -205,7 +205,7 @@ public class Utilisateur implements UserDetails {
      */
     @Override
     public boolean isEnabled() {
-        return validerInscription;;
+        return validerInscription;
     }
 
     /**
