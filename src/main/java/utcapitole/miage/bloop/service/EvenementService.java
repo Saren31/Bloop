fb45b6a9889b0cb382ba76726bea4f6c4ae4b561
yan4093L <cyly1002@gmail.com>
2025-05-20 15:12:23 +0200

    commit

--- src/main/java/utcapitole/miage/bloop/service/EvenementService.java
+++ src/main/java/utcapitole/miage/bloop/service/EvenementService.java
@@ -3,6 +3,7 @@ package utcapitole.miage.bloop.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import utcapitole.miage.bloop.model.entity.Evenement;
+import utcapitole.miage.bloop.model.entity.Utilisateur;
 import utcapitole.miage.bloop.repository.EvenementRepository;
 
 import java.util.List;
@@ -23,5 +24,50 @@ public class EvenementService {
 
     public List<Evenement> getEvenementsParOrganisateur(Long idUser) {
         return evenementRepository.findByOrganisateur_IdUser(idUser);
+    }
+
+    public Evenement getEvenementParId(Long id) {
+        return evenementRepository.findById(id).orElse(null);
+    }
+
+    public void inscrireUtilisateur(Evenement evenement, Utilisateur utilisateur) {
+        if (!evenement.getParticipants().contains(utilisateur)) {
+            evenement.getParticipants().add(utilisateur);
+            evenementRepository.save(evenement);
+        }
+    }
+
+    public void retirerUtilisateur(Evenement evenement, Utilisateur utilisateur) {
+        if (evenement.getParticipants().contains(utilisateur)) {
+            evenement.getParticipants().remove(utilisateur);
+            evenementRepository.save(evenement);
+        }
+    }
+
+    public void marquerInteresse(Evenement evenement, Utilisateur utilisateur) {
+        if (!evenement.getInteresses().contains(utilisateur)) {
+            evenement.getInteresses().add(utilisateur);
+            evenementRepository.save(evenement);
+        }
+    }
+
+    public void retirerInteresse(Evenement evenement, Utilisateur utilisateur) {
+        if (evenement.getInteresses().contains(utilisateur)) {
+            evenement.getInteresses().remove(utilisateur);
+            evenementRepository.save(evenement);
+        }
+    }
+
+    public boolean estInscrit(Evenement evenement, Utilisateur utilisateur) {
+
+        return evenement.getInscrits().contains(utilisateur);
+    }
+
+    public boolean estInteresse(Evenement evenement, Utilisateur utilisateur) {
+
+        return evenement.getInteresses().contains(utilisateur);
+    }
+
+    public void ajouterParticipant(Evenement evenement, Utilisateur utilisateur) {
     }
 }
